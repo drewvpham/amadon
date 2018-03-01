09a91cacd7a7935542f105e758203759ef0e44ee
Drew Pham <drewvpham@gmail.com>
2018-02-28 19:50:49 -0600

    inital commit;

---
+++ apps/amadon/urls.py
@@ -0,0 +1,13 @@
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="landing"),
+
+    url(r'^register$', views.register, name="register"),
+    url(r'^login$', views.login, name="login"),
+    url(r'^logout$', views.logout, name="logout"),
+
+    # Another app to defined a dashboard route
+]
